c53d4bc1ebf6c295bd0ab933ec9ba5d8ebea378f
Marcell Leleszi <marcell.leleszi@hiya.com>
2024-06-14 11:00:24 +0200

    Add EXISTS tests

--- tests/controller_test.cpp
+++ tests/controller_test.cpp
@@ -144,7 +144,7 @@ TEST(ControllerTests, HandleGET) {
     ASSERT_EQ(bulkData, stringToByteVector("val"));
 }
 
-TEST(ControllerTests, HandleGETEndWithNum) {
+TEST(ControllerTests, HandleGETEndsWithNum) {
     Controller controller;
 
     std::vector<RedisType::BulkString> setCommand{
@@ -166,4 +166,77 @@ TEST(ControllerTests, HandleGETEndWithNum) {
     auto bulkData = *std::get<RedisType::BulkString>(result).data;
 
     ASSERT_EQ(bulkData, stringToByteVector("val2"));
+}
+
+TEST(ControllerTests, HandleExistsNotExists) {
+    Controller controller;
+
+    std::vector<RedisType::BulkString> command{
+            RedisType::BulkString("EXISTS"),
+            RedisType::BulkString("key"),
+    };
+
+    auto result = controller.handleCommand(command);
+
+    ASSERT_TRUE(std::holds_alternative<RedisType::Integer>(result));
+    auto bulkData = std::get<RedisType::Integer>(result).data;
+
+    ASSERT_EQ(bulkData, 0);
+}
+
+TEST(ControllerTests, HandleExistsSingleKey) {
+    Controller controller;
+
+    std::vector<RedisType::BulkString> setCommand{
+            RedisType::BulkString("SET"),
+            RedisType::BulkString("key"),
+            RedisType::BulkString("val"),
+    };
+
+    controller.handleCommand(setCommand);
+
+    std::vector<RedisType::BulkString> existsCommand{
+            RedisType::BulkString("EXISTS"),
+            RedisType::BulkString("key"),
+    };
+
+    auto result = controller.handleCommand(existsCommand);
+
+    ASSERT_TRUE(std::holds_alternative<RedisType::Integer>(result));
+    auto bulkData = std::get<RedisType::Integer>(result).data;
+
+    ASSERT_EQ(bulkData, 1);
+}
+
+TEST(ControllerTests, HandleExistsMultipleKeys) {
+    Controller controller;
+
+    std::vector<RedisType::BulkString> setCommand{
+            RedisType::BulkString("SET"),
+            RedisType::BulkString("key"),
+            RedisType::BulkString("val"),
+    };
+
+    std::vector<RedisType::BulkString> setCommand2{
+            RedisType::BulkString("SET"),
+            RedisType::BulkString("key2"),
+            RedisType::BulkString("val2"),
+    };
+
+    controller.handleCommand(setCommand);
+    controller.handleCommand(setCommand2);
+
+    std::vector<RedisType::BulkString> existsCommand{
+            RedisType::BulkString("EXISTS"),
+            RedisType::BulkString("key"),
+            RedisType::BulkString("key2"),
+            RedisType::BulkString("key3"),
+    };
+
+    auto result = controller.handleCommand(existsCommand);
+
+    ASSERT_TRUE(std::holds_alternative<RedisType::Integer>(result));
+    auto bulkData = std::get<RedisType::Integer>(result).data;
+
+    ASSERT_EQ(bulkData, 2);
 }
